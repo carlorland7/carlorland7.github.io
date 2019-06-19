e1add12e09e797d743d321beab229bcd24c771f5
Carl Orland Sabino <34226191+carlorland7@users.noreply.github.com>
2019-06-20 01:45:06 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function hoverFb(element){
+    element.setAttribute('src', 'images/facebook-yellow.png');
+}
+
+function unhoverFb(element){
+    element.setAttribute('src', 'images/facebook.png');
+}
+
+function hoverTw(element){
+    element.setAttribute('src', 'images/twitter-yellow.png');
+}
+
+function unhoverTw(element){
+    element.setAttribute('src', 'images/twitter.png');
+}
+
+function hoverIg(element){
+    element.setAttribute('src', 'images/instagram-yellow.png');
+}
+
+function unhoverIg(element){
+    element.setAttribute('src', 'images/instagram.png');
+}
